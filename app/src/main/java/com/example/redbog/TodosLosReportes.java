5a95e2db2696635beb9ecaf8919d37addb6901fd
eduran777 <eduran@unal.edu.co>
2022-10-07 12:52:55 -0500

    Se Guardan los reportes, se ven generales y mis reportes version 2

--- app/src/main/java/com/example/redbog/TodosLosReportes.java
+++ app/src/main/java/com/example/redbog/TodosLosReportes.java
@@ -18,6 +18,8 @@ import com.example.redbog.clases.ListAdapter;
 import com.example.redbog.clases.ListAdapter2;
 import com.example.redbog.clases.Reporte;
 import com.example.redbog.stack.StackRef;
+import com.example.redbog.queue.Cola;
+import com.example.redbog.queue.Node;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,7 @@ public class TodosLosReportes extends AppCompatActivity {
 
 
     StackRef<Reporte> misReportesStack;
+    Cola<Reporte> misReportesCola;
     List<Reporte> misReportesLista;
     String celular;
     Dialog dialog;
@@ -38,9 +41,10 @@ public class TodosLosReportes extends AppCompatActivity {
         celular = getIntent().getStringExtra("celular");
         setContentView(R.layout.activity_todos_los_reportes);
         dialog = new Dialog(this);
-        misReportesStack = llenarStack();
-        Toast.makeText(this, "empty stack" + misReportesStack.isEmpty(), Toast.LENGTH_SHORT).show();
-        stackaLista(misReportesStack );
+        //misReportesStack = llenarStack();
+        misReportesCola = llenarCola();
+        Toast.makeText(this, "empty queue" + misReportesCola.empty(), Toast.LENGTH_SHORT).show();
+        stackaLista(misReportesCola);
         init();
         llenarStack();
 
@@ -62,10 +66,34 @@ public class TodosLosReportes extends AppCompatActivity {
         }
         return misReportesStack;
     }
+    public Cola<Reporte> llenarCola(){
+        misReportesCola = new Cola<Reporte>();
+        Reporte reporte;
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,7);
+        SQLiteDatabase BaseDeDatos = admin.getReadableDatabase();
+
+        Cursor fila = BaseDeDatos.rawQuery("select * from reporte ",null);
+        while(fila.moveToNext()){
+            Toast.makeText(this,"texto" +fila.getString(4),Toast.LENGTH_SHORT).show();
+            //String nombre, String reporte, String fecha, String hora, String localidad, String tipologia
+            reporte = new Reporte(fila.getString(7), fila.getString(4),fila.getString(5),fila.getString(6),fila.getString(3),fila.getString(2),fila.getInt(0));
+            misReportesCola.enqueue(reporte);
+        }
+
+        return misReportesCola;
+
+    }
     public void stackaLista(StackRef<Reporte> stack){
         misReportesLista = new ArrayList<>();
         while(!stack.isEmpty()){
             misReportesLista.add(stack.pop());
+        }
+
+    }
+    public void stackaLista(Cola<Reporte> cola){
+        misReportesLista = new ArrayList<>();
+        while(!cola.empty()){
+            misReportesLista.add(cola.dequeue());
         }
 
     }
